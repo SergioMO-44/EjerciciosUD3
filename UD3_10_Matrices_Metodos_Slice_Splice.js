69eb34862e00659674fe90ef29b916d2e409760a
SergioMO-44 <semarolmos@gmail.com>
2024-10-27 17:44:29 +0100

    Create UD3_10_Matrices_Metodos_Slice_Splice.js

---
+++ UD3_10_Matrices_Metodos_Slice_Splice.js
@@ -0,0 +1,12 @@
+// DECLARACION indices 0,1,2,3
+let animales = ["perro", "gato", "pajaro", "pez"];
+
+// SLICE = Corte;
+// let corteSlice = animales.splice(2, 1); // corta los elementos: indice , numero de elementos a cortar
+// console.log(corteSlice);
+// console.log(animales);
+
+//SPLICE = Corte + Substitucion
+// let corteSplice = animales.splice(2, 1, "mosquito", "mosca"); // añade elementos (indice , numero de elementos a cortar, nuevos elementos a añadir)
+// console.log(corteSplice);
+// console.log(animales);
