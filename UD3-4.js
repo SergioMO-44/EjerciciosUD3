c6ab29f394d05a4c69ff2da270de314c9f97a51b
SergioMO-44 <semarolmos@gmail.com>
2024-10-27 17:44:05 +0100

    Create UD3-4.js

---
+++ UD3-4.js
@@ -0,0 +1,8 @@
+let vector = [1, 2, 8, 4, 2, 1, 8, 3, 2, 4, 2];
+for (let i = 0; i < vector.length; i++) {
+  //let value = vector[i];
+  // console.log(value,i)
+  if (vector[i] % 2 == 0) {
+    console.log(" El valor es " + vector[i], "y la posicion es " + i);
+  }
+}
