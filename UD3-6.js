2b569b1104950ffa8c40be70e900f47498e520b2
SergioMO-44 <semarolmos@gmail.com>
2024-10-27 17:44:11 +0100

    Create UD3-6.js

---
+++ UD3-6.js
@@ -0,0 +1,7 @@
+let vector = [1, 2, 8, 4, 2, 1, 8, 3, 2, 4, 2];
+let cuenta = 0;
+for (let i = 0; i < vector.length; i++) {
+    //console.log(vector[i],cuenta);
+    cuenta = cuenta + vector[i]
+}
+console.log("El total es " + cuenta + ".");
